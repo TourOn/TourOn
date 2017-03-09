1bcc2a5faeaa3e1b3af1f95d4c2dd5ad4b7048e4
Joseph Coppola <jbcoppola@gmail.com>
2017-03-09 10:51:43 -0500

    updated database to remove AccountType table and updated Register controller to enter data to specific types of accounts

--- TourOn/Models/AccountViewModels.cs
+++ TourOn/Models/AccountViewModels.cs
@@ -3,152 +3,163 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TourOn.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
+	public class ExternalLoginConfirmationViewModel
+	{
+		[Required]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+	}
 
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
+	public class ExternalLoginListViewModel
+	{
+		public string ReturnUrl { get; set; }
+	}
 
-    public class SendCodeViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
-        public bool RememberMe { get; set; }
-    }
+	public class SendCodeViewModel
+	{
+		public string SelectedProvider { get; set; }
+		public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+		public string ReturnUrl { get; set; }
+		public bool RememberMe { get; set; }
+	}
 
-    public class VerifyCodeViewModel
-    {
-        [Required]
-        public string Provider { get; set; }
+	public class VerifyCodeViewModel
+	{
+		[Required]
+		public string Provider { get; set; }
 
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-        public string ReturnUrl { get; set; }
+		[Required]
+		[Display(Name = "Code")]
+		public string Code { get; set; }
+		public string ReturnUrl { get; set; }
 
-        [Display(Name = "Remember this browser?")]
-        public bool RememberBrowser { get; set; }
+		[Display(Name = "Remember this browser?")]
+		public bool RememberBrowser { get; set; }
 
-        public bool RememberMe { get; set; }
-    }
+		public bool RememberMe { get; set; }
+	}
 
-    public class ForgotViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
+	public class ForgotViewModel
+	{
+		[Required]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+	}
 
-    public class LoginViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+	public class LoginViewModel
+	{
+		[Required]
+		[Display(Name = "Email")]
+		[EmailAddress]
+		public string Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
-    }
+		[Display(Name = "Remember me?")]
+		public bool RememberMe { get; set; }
+	}
 
-    public class RegisterViewModel
-    {
-        [Required]
-        public char FormType { get; set; }
+	public class RegisterViewModel
+	{
+		[Required]
+		public string FormType { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Account contact email")]
+		public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm password")]
+		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+		[Required]
+		public string Name { get; set; }
 
-        [Required]
-        [Display(Name = "Street")]
-        public string Street { get; set; }
+		[Required]
+		[Display(Name = "City")]
+		public string City { get; set; }
 
-        [Required]
-        [Display(Name = "City")]
-        public string City { get; set; }
+		[Required]
+		[Display(Name = "State")]
+		public string State { get; set; }
 
-        [Required]
-        [Display(Name = "State")]
-        public string State { get; set; }
+		[Required]
+		[Display(Name = "Public email")]
+		public string PublicEmail { get; set; }
 
-        [Required]
-        [Display(Name = "Zip Code")]
-        public int Zip { get; set; }
+		[Required]
+		[Phone]
+		[Display(Name = "Phone Number")]
+		public string Phone { get; set; }
 
-        [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string Phone { get; set; }
+		public string Region { get; set; }
 
-        [Required]
-        public string Region { get; set; }
+		[Display(Name = "Genre")]
+		public string Genre { get; set; }
 
-        [Required]
-        [Display(Name = "Genre")]
-        public string Genre { get; set; }
+		//Venue-specific fields
+		[Display(Name = "Street")]
+		public string Street { get; set; }
 
-        [Required]
-        [Display(Name = "Capacity")]
-        public int Capacity { get; set; }
+		[Display(Name = "Zip Code")]
+		public int Zip { get; set; }
 
-        [Required]
-        [Display(Name = "Band Size")]
-        public int Size { get; set; }
-    }
+		[Display(Name = "Capacity")]
+		public int Capacity { get; set; }
 
-    public class ResetPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+		public string Equipment { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+		public string Parking { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+		//Band-specific fields
+		[Display(Name = "Members in band")]
+		public int Size { get; set; }
+		
+		public string Showcase { get; set; }
 
-        public string Code { get; set; }
-    }
 
-    public class ForgotPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
+		public static readonly byte AdminAccountType = 0;
+		public static readonly byte BandAccountType = 1;
+		public static readonly byte VenueAccountType = 2;
+	}
+
+	public class ResetPasswordViewModel
+	{
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm password")]
+		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
+
+		public string Code { get; set; }
+	}
+
+	public class ForgotPasswordViewModel
+	{
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+	}
 }
--- TourOn/Models/IdentityModels.cs
+++ TourOn/Models/IdentityModels.cs
@@ -8,65 +8,65 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TourOn.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
+	// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+	public class ApplicationUser : IdentityUser
+	{
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+		{
+			// Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+			var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+			// Add custom user claims here
+			return userIdentity;
+		}
 
-        [Required]
-        public bool IsBand { get; set; }
 
-        //links region to application user
-        public virtual Region Region { get; set; }
+		//links region to application user
+		public virtual Region Region { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+		[Required]
+		public string Name { get; set; }
 
-        [Required]
-        public string Description { get; set; }
+		[Required]
+		public string Description { get; set; }
 
-        //links genre to application user
-        public virtual Genre Genre { get; set; }
+		//links genre to application user
+		public virtual Genre Genre { get; set; }
 
-        [Required]
-        [Phone]
-        public string Phone { get; set; }
+		[Required]
+		[Phone]
+		public string Phone { get; set; }
 
-        [Required]
-        public string City { get; set; }
+		[Required]
+		public string City { get; set; }
 
-        [Required]
-        public string State { get; set; }
+		[Required]
+		public string State { get; set; }
 
-        [Required]
-        [EmailAddress]
-        public string PublicEmail { get; set; }
+		[Required]
+		[EmailAddress]
+		public string PublicEmail { get; set; }
 
-        //other websites
-        public string OtherContacts { get; set; }
+		//other websites
+		public string OtherContacts { get; set; }
 
-        //links comments and pictures to application user
-        public virtual ICollection<Comment> Comments { get; set; }
-        public virtual ICollection<Picture> Pictures { get; set; }
+		//links comments and pictures to application user
+		public virtual ICollection<Comment> Comments { get; set; }
+		public virtual ICollection<Picture> Pictures { get; set; }
 
-    }
+		//determines account type
+		public byte AccountType { get; set; }
+	}
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false)
+		{
+		}
 
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+	}
 }
