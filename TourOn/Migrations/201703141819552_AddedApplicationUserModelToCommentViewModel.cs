1290c459b6c088e40666d2d1705735c15343e33a
Satnam Brar <satnamsbrar@gmail.com>
2017-03-14 14:39:37 -0400

    created CommentViewModel to link Comment, Band, Venue, and ApplicationUser models.

---
+++ TourOn/Migrations/201703141819552_AddedApplicationUserModelToCommentViewModel.cs
@@ -0,0 +1,16 @@
+namespace TourOn.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedApplicationUserModelToCommentViewModel : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ TourOn/Models/CommentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TourOn.Models
+{
+    public class CommentViewModel
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public Band Band { get; set; }
+        public Venue Venue { get; set; }
+        public Comment Comment { get; set; }
+    }
+}
