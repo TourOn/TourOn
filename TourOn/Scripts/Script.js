a9b3bc23085ea18e8a595e46dd51ff4b83be5293
Joseph Coppola <jbcoppola@gmail.com>
2017-03-23 13:20:49 -0400

    updated comment section to use like buttons and toggle function

--- TourOn/Scripts/Script.js
+++ TourOn/Scripts/Script.js
@@ -1,6 +1,21 @@
 ﻿function showCreateCommentForm() {
     document.getElementById("NewCommentForm").style.display = "block";
+    document.getElementById("new-comment").style.display = "none";
 }
 function hideCreateCommentForm() {
     document.getElementById("NewCommentForm").style.display = "none";
+    document.getElementById("new-comment").style.display = "block";
 }
+
+$(document).ready(function () {
+    $("#like").click(function () {
+        $("#ThumbsUp").val("True")
+        $("#like").css("border-width", "3px")
+        $("#dislike").css("border-width", "1px")
+    });
+    $("#dislike").click(function () {
+        $("#ThumbsUp").val("False")
+        $("#dislike").css("border-width", "3px")
+        $("#like").css("border-width", "1px")
+    });
+});
